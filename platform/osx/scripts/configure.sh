f771c76860023f54e20b9885f659723c053fecff
Bruno de Oliveira Abinader <bruno@mapbox.com>
2016-01-11 17:08:13 +0200

    [osx] Added default frameworks for non-GLFW build

--- platform/osx/scripts/configure.sh
+++ platform/osx/scripts/configure.sh
@@ -13,3 +13,8 @@ VARIANT_VERSION=1.1.0
 RAPIDJSON_VERSION=1.0.2
 GTEST_VERSION=1.7.0
 PIXELMATCH_VERSION=0.9.0
+
+function print_opengl_flags {
+    CONFIG+="    'opengl_cflags%': [],"$LN
+    CONFIG+="    'opengl_ldflags%': ['-framework OpenGL', '-framework CoreFoundation'],"$LN
+}

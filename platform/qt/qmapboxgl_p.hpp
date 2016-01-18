d974b95053a2860765e032ca25d8149dc8d36516
Thiago Marcos P. Santos <thiago@mapbox.com>
2016-01-18 13:32:47 +0200

    [Qt] Add support for margins

--- platform/qt/qmapboxgl_p.hpp
+++ platform/qt/qmapboxgl_p.hpp
@@ -4,6 +4,7 @@
 #include <mbgl/map/map.hpp>
 #include <mbgl/map/view.hpp>
 #include <mbgl/storage/default_file_source.hpp>
+#include <mbgl/util/geo.hpp>
 #include <mbgl/util/run_loop.hpp>
 
 #include <QObject>
@@ -35,6 +36,7 @@ public:
     void afterRender() final {}
     void notifyMapChange(mbgl::MapChange change) final;
 
+    mbgl::EdgeInsets margins;
     QSize size;
 
     QMapboxGL *q_ptr = nullptr;
